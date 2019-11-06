6aeb994170bbe60e3ac99c065f310447a1c8ea50
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 15:30:14 -0600

    Add files via upload

---
+++ Logger/LogTrace.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace LogTrace
+{
+    public class LogTrace
+    {
+        static string connectionstring;
+        static LogTrace()
+        {
+            try
+            {
+                connectionstring = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString;
+            }
+            catch (Exception ex) 
+            {
+
+                throw new Exception("Error while getting DefaultConnectionString for Logger");
+            }
+        }
+        public static void Log(Exception ex)
+        {
+            try
+            {
+                using(System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(connectionstring))
+                {
+                    con.Open();
+                    using(var com = con.CreateCommand())
+                    {
+                        com.CommandText = "InsertLogItem";
+                        com.CommandType = System.Data.CommandType.StoredProcedure;
+                        com.Parameters.AddWithValue("@Message", ex.Message);
+                        com.Parameters.AddWithValue("@Trace", ex.StackTrace.ToString());
+                        com.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+
+                var p = System.Web.HttpContext.Current.Server.MapPath("~");
+                p += "ErrorLog.Log";
+                System.IO.File.AppendAllText(p, "while attempting to record the original exception to the database, this exception occurred\r\n");
+                System.IO.File.AppendAllText(p, exc.ToString());
+                System.IO.File.AppendAllText(p, "This is the Original Exception that was attempting to be written to the database\r\n");
+                System.IO.File.AppendAllText(p, ex.ToString());
+            }
+        }
+    }
+}
---
+++ LumberJack/Logger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LumberJack
+{
+    public class Logger
+    {
+        //In this the connection string is loaded only loaded 1 time in the start as 
+        //static.
+        static string connectionstring;
+        static Logger()
+        {
+            try
+            {
+                connectionstring = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString;
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Something went wrong while getting the DefaultConnectionString for Logger");
+            }
+        }
+        // I have this method static so that I will be able to have semantics like 
+        //Console.WriteLine.
+        public static void Log(Exception ex)
+        {
+            try
+            {
+                using (System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(connectionstring))
+                {
+                    con.Open();
+                    using (var com = con.CreateCommand())
+                    {
+                        com.CommandText = "Logger";
+                        com.CommandType = System.Data.CommandType.StoredProcedure;
+                        com.Parameters.AddWithValue("@Message", ex.Message);
+                        com.Parameters.AddWithValue("@Stacktrace", ex.StackTrace.ToString());
+                        com.ExecuteNonQuery();
+                    }
+                }
+            }
+            // If this is our failsafe if the databse is down
+            catch (Exception exc)
+            {
+                var p = System.Web.HttpContext.Current.Server.MapPath("~");
+                p += @"ErrorLog.Log";
+                System.IO.File.AppendAllText(p,
+                "while attempting to record the original exception to the database, this exception occurred\r\n");
+                System.IO.File.AppendAllText(p, exc.ToString());
+                System.IO.File.AppendAllText(p,
+                "This is the Original Exception that was attempting to be written to the database\r\n");
+                System.IO.File.AppendAllText(p, ex.ToString());
+
+            }
+        }
+    }
+}
